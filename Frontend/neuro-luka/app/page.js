a14ff9731da5164fbd03dec33ea0a3de55828317
Tougashi <94227628+Tougashi@users.noreply.github.com>
2025-05-05 00:00:13 +0700

    Update page.js

--- Frontend/neuro-luka/app/page.js
+++ Frontend/neuro-luka/app/page.js
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Sosmed from "@/components/Sosmed";
+import About from "@/components/About";
 import 'remixicon/fonts/remixicon.css';
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Sosmed />
+      <About />
       <div className="container mx-auto px-4">
         {/* <h1>Yogi</h1> */}
       </div>
